d879937755598ce92763b42928c781f2b27d5d37
Emmanuel Valaquio <emmanuel.valaquio@gengo.com>
2020-07-31 22:20:47 +0900

    Updates

---
+++ precache-manifest.cba3e0f916854a8d14a27f21097ec49c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3bbc35b2e7d2a0c3e59bce79cc6c6f",
+    "url": "/content/index.html"
+  },
+  {
+    "revision": "2df8408abb67a50d9064",
+    "url": "/content/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18fc026acbf84e8ce0f1",
+    "url": "/content/static/js/2.de548e81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/content/static/js/2.de548e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df8408abb67a50d9064",
+    "url": "/content/static/js/main.2ce9a1d9.chunk.js"
+  },
+  {
+    "revision": "c25c8570055ae9584c4d",
+    "url": "/content/static/js/runtime-main.23553fa4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/content/static/media/logo.5d5d9eef.svg"
+  }
+]);
